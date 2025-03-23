ef27cfb1109c0a5a0bfc741e13f7f73c8f409e48
seanat0r <seanatorhd@gmail.com>
2025-03-23 08:28:45 +0100

    finished fetching data from API and set it up a obj for the data

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@
 import "core-js"; 
 import "regenerator-runtime/runtime"; 
 
+import { WeatherAPI } from "./js/getWeatherAPI";
+
 // Importing CSS
 import "./styles/reset.css";
 import "./styles/style.css";
 
 //Your code here
+const weather = new  WeatherAPI;
+weather.procceed("Erlinsbach");
--- src/js/getWeatherAPI.js
+++ src/js/getWeatherAPI.js
@@ -1,22 +1,48 @@
 //* https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/[location]/[date1]/[date2]?key=YOUR_API_KEY
 export class WeatherAPI {
-	async #getAPI(location) {
+	#weatherData = {
+		location: "",
+		temperature: null,
+		feelsLike: null,
+		humidity: null,
+		weatherDescription: "",
+		weatherIcon: null,
+		tomorowTemperature: null,
+	};
+	async #fetchWeatherData(location) {
 		try {
 			const api = await fetch(
 				`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/?key=EJG8Y77YC3TFC3Z8GYF63TFBY`,
 				{ mode: "cors" }
 			);
 			console.log(api);
-            const weatherData = await api.json();
-            console.log(weatherData);
-            console.log(weatherData.currentConditions.temp);
-
+			const weatherData = await api.json();
+			console.log(weatherData);
+			return weatherData;
 		} catch (error) {
 			console.error("Error in WeatherAPI; getAPI(): ", error);
+			throw error;
 		}
 	}
 
-	procceed(location) {
-		this.#getAPI(location);
+	async #setUpWeatherData(weatherInformaton, location) {
+		this.#weatherData.location = location;
+		this.#weatherData.temperature = weatherInformaton.currentConditions.temp;
+		this.#weatherData.feelsLike = weatherInformaton.currentConditions.feelslike;
+		this.#weatherData.humidity = weatherInformaton.currentConditions.humidity;
+		this.#weatherData.weatherDescription = weatherInformaton.description;
+		this.#weatherData.weatherIcon = weatherInformaton.currentConditions.icon;
+		this.#weatherData.tomorowTemperature = weatherInformaton.days[1].temp;
+		console.log(this.#weatherData);
+	}
+
+	async procceed(location) {
+		try {
+			const weatherInformaton = await this.#fetchWeatherData(location);
+			await this.#setUpWeatherData(weatherInformaton, location);
+		} catch (error) {
+			console.error("Error in WeatherAPI; procceed(): ", error);
+			throw error;
+		}
 	}
 }
